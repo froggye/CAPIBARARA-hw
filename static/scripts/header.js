e8d6822eb15ca015a03d43723d77938f8865aca7
Кузнецова Мария Дмитриевна [012751] <012751@pnu.edu.ru>
2023-11-23 18:10:07 +1000

    upd - правки в js, доработка меню в адаптиве

--- static/scripts/header.js
+++ static/scripts/header.js
@@ -15,34 +15,47 @@ function openMenu () {
     headerTop.classList.toggle('menu-expanded');
     headerMenu.classList.toggle('menu-expanded');
 
-    if (header.classList.contains('menu-expanded')){
-        heroSection.style.marginTop = height + 'px';
-    }
-    else {
-        heroSection.style.marginTop = '0';
-    }
+    heroSection.style.marginTop = header.classList.contains('menu-expanded')
+        ? height + 'px'
+        : '0';
 }
 
 const menuButton = document.getElementById("menu-toggle");
 menuButton.addEventListener("click", openMenu);
 
-window.onresize = function(event) {
-    if (window.innerWidth > 768) {
-        if (header.classList.contains('menu-expanded')){
-            header.classList.remove('menu-expanded');
-        }
-        if (headerWrapper.classList.contains('drop-shadow--2xl')){
-            headerWrapper.classList.remove('drop-shadow--2xl');
-        }
-        if (headerWrapper.classList.contains('menu-expanded')){
-            headerWrapper.classList.remove('menu-expanded');
-        }
-        if (headerTop.classList.contains('menu-expanded')){
-            headerTop.classList.remove('menu-expanded');
-        }
-        if (headerMenu.classList.contains('menu-expanded')){
-            headerMenu.classList.remove('menu-expanded');
-        }
-        heroSection.style.marginTop = '0';
+function removeHeaderClasses () {
+    if (header.classList.contains('menu-expanded')){
+        header.classList.remove('menu-expanded');
     }
-};
+    if (headerWrapper.classList.contains('drop-shadow--2xl')){
+        headerWrapper.classList.remove('drop-shadow--2xl');
+    }
+    if (headerWrapper.classList.contains('menu-expanded')){
+        headerWrapper.classList.remove('menu-expanded');
+    }
+    if (headerTop.classList.contains('menu-expanded')){
+        headerTop.classList.remove('menu-expanded');
+    }
+    if (headerMenu.classList.contains('menu-expanded')){
+        headerMenu.classList.remove('menu-expanded');
+    }
+    heroSection.style.marginTop = '0';
+}
+
+window.onresize = function(event) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        return;
+    }
+    removeHeaderClasses();
+}
+
+const menuItems = document.getElementsByClassName("header__menu-item");
+
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function(){
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            return;
+        }
+        removeHeaderClasses();
+    });
+}
